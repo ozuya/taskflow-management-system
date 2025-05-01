be4aa117c04a2a5dddba357b5074c0c59438f53c
Cedrick Gayoso <cedrick.gayoso@gmail.com>
2025-05-01 19:57:28 +0800

    Add files via upload

---
+++ taskflow_db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE taskflow_db;
+
+USE taskflow_db;
+
+CREATE TABLE tasks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
